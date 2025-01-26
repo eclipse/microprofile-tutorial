16e0be8b5540d892202a210619c38abbcf173746
Tarun Telang <tarun.telang@gmail.com>
2025-01-26 10:51:02 +0530

    Source code for chapter 05
    Uploading source code for chapter 05

---
+++ code/chapter05/catalog/src/main/java/io/microprofile/tutorial/store/product/repository/ProductRepository.java
@@ -0,0 +1,51 @@
+package io.microprofile.tutorial.store.product.repository;
+
+import java.util.List;
+
+import io.microprofile.tutorial.store.product.entity.Product;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@RequestScoped
+public class ProductRepository {
+
+    @PersistenceContext(unitName = "product-unit")
+    private EntityManager em;
+
+    @Transactional
+    public void createProduct(Product product) {
+        em.persist(product);
+    }
+
+    @Transactional
+    public Product updateProduct(Product product) {
+        return em.merge(product);
+    }
+
+    @Transactional
+    public void deleteProduct(Product product) {
+        em.remove(product);
+    }
+
+    @Transactional
+    public List<Product> findAllProducts() {
+        return em.createNamedQuery("Product.findAllProducts", 
+        Product.class).getResultList();
+    }
+
+    @Transactional
+    public Product findProductById(Long id) {
+        return em.find(Product.class, id);
+    }
+
+    @Transactional
+    public List<Product> findProduct(String name, String description, Double price) {
+        return em.createNamedQuery("Event.findProduct", Product.class)
+            .setParameter("name", name)
+            .setParameter("description", description)
+            .setParameter("price", price).getResultList();
+    }
+
+}
---
+++ code/chapter05/payment/src/main/java/io/microprofile/tutorial/store/payment/service/PaymentService.java
@@ -0,0 +1,59 @@
+package io.microprofile.tutorial.store.payment.service;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
+import io.microprofile.tutorial.store.payment.entity.PaymentDetails;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/authorize")
+@RequestScoped
+public class PaymentService {
+    
+    @Inject
+    @ConfigProperty(name = "payment.gateway.apiKey", defaultValue = "secret_api_key")
+    private String apiKey;
+
+    @Inject
+    @ConfigProperty(name = "payment.gateway.endpoint", defaultValue = "https://api.paymentgateway.com")
+    private String endpoint;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "process payment", description = "Processes payment using a payment gateway")
+    @APIResponses(value = {
+        @APIResponse(
+            responseCode = "200",
+            description = "Payment processed successfully",
+            content = @Content(mediaType = "application/json")
+        ),
+        @APIResponse(
+            responseCode = "400",
+            description = "Payment processing failed",
+            content = @Content(mediaType = "application/json")
+        )
+    })
+    public Response processPayment(PaymentDetails paymentDetails) {
+
+        // Example logic to call the payment gateway API
+        System.out.println();
+        System.out.println("Calling payment gateway API at: " + endpoint + "with API key: " + apiKey);
+        // Here, assume a successful payment operation for demonstration purposes
+        // Actual implementation would involve calling the payment gateway and handling the response
+        
+        // Dummy response for successful payment processing
+        String result = "{\"status\":\"success\", \"message\":\"Payment processed successfully.\"}";
+        return Response.ok(result, MediaType.APPLICATION_JSON).build();
+    }
+}
