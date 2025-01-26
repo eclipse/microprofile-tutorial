4dd2079543514965a6e3651a40725c38fc6785c5
Tarun Telang <tarun.telang@gmail.com>
2025-01-26 10:28:20 +0530

    Delete code/chapter04 directory

--- code/chapter04/mp-ecomm-store/mp-ecomm-store/src/main/java/io/microprofile/tutorial/store/product/ProductRestApplication.java
+++
@@ -1,9 +0,0 @@
-package io.microprofile.tutorial.store.product;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class ProductRestApplication extends Application{
-
-}
